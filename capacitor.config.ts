84a23a92cf84507a65e720156a446fdd839b5c68
jchraibi <jchraibi@redhat.com>
2023-02-15 22:44:44 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'coolstore-mobile',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
