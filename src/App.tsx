e5ccc4407c130871467b2b45ad979c13145c17d8
Jaafar Chraibi <jchraibi@redhat.com>
2023-02-20 16:26:07 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import ViewMessage from './pages/ViewMessage';
 
+/* new code */ 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
